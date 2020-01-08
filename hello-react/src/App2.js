a19a1eb355076edb53e600c75613c09801684db6
choi solji <bssj9307@gmail.com>
2020-01-08 19:49:40 +0900

    200108 | day2 | component

--- hello-react/src/App2.js
+++ hello-react/src/App2.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 // 클래스형 컴포넌트
+import MyComponent from './components/MyComponent';
+// MyComponent 컴포넌트를 불러왔다.
 
 // 클래스형 컴포넌트와 함수형 컴포넌트의 역할은 같다.
 /*
@@ -13,11 +15,12 @@ import React, {Component} from 'react';
     2. 메모리를 덜 사용한다.
     3. Hooks 업데이트로 state 와 라이프사이클도 사용가능하다. (클래스형과 방식은 다르다.)
     4. React 는 함수형 컴포넌트 + Hooks 를 권장한다.
- */
+*/
 class App extends Component {
     render() {  // 함수형 컴포넌트에서 필수!
         const name = 'react';
-        return <div className="react">{name}</div>;
+        // return <div className="react">{name}</div>;
+        return <MyComponent/>
     }
 }
 export default App;
